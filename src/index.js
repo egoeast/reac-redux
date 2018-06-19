83e002da6fec1f1c39ee840e390178981f9db0b9
andrei_deshkevich <andrei.deshkevich@gmail.com>
2018-06-19 18:45:59 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-document.getElementById('root').innerHTML = 'Привет, я готов!'
-module.hot.accept()
+import 'babel-polyfill'
+import React from 'react'
+import {render} from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+
+const store = createStore( () => {}, {})
+
+render(
+    <Provider>
+    <App />
+    </Provider>,
+    document.getElementById('root')
+)
